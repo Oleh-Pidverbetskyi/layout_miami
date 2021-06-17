1674dfaa814e0ea792164fcefde85d7b22e7a683
Oleh Pidverbetskyi <PidverbetskiyOleg1@gmail.com>
2021-06-17 15:03:09 +0300

    miami header+menu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const burger = document.querySelector('.icon');
+const menu = document.querySelector('.nav');
+const close = document.querySelector('.icon--close');
+
+burger.addEventListener('click', () => {
+  menu.classList.add('nav--active');
+});
+
+close.addEventListener('click', () => {
+  menu.classList.remove('nav--active');
+});
