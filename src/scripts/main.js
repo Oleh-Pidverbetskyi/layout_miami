15032058d41f7d3ab1de1756c2ddecfa0049c195
Oleh Pidverbetskyi <PidverbetskiyOleg1@gmail.com>
2021-06-18 23:57:01 +0300

    100% solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,6 +3,11 @@
 const burger = document.querySelector('.icon');
 const menu = document.querySelector('.nav');
 const close = document.querySelector('.icon--close');
+const main = document.querySelector('.main');
+
+main.addEventListener('click', () => {
+  menu.classList.remove('nav--active');
+});
 
 burger.addEventListener('click', () => {
   menu.classList.add('nav--active');
@@ -10,4 +15,25 @@ burger.addEventListener('click', () => {
 
 close.addEventListener('click', () => {
   menu.classList.remove('nav--active');
+});
+
+const links = document.querySelectorAll('.nav__link');
+
+links.forEach((item) =>
+  item.addEventListener('click', () => {
+    if (menu.classList.contains('nav--active')) {
+      menu.classList.toggle('nav--active');
+    }
+  })
+);
+
+const form = document.querySelector('.form');
+const inputs = document.querySelectorAll('.form-fieldsize');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  inputs.forEach((input) => {
+    input.value = '';
+  });
 });
